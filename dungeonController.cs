da70bee88502090b2f19c865519445a3c2867445
gennawho <genevieve.rector@cuw.edu>
2024-02-23 07:23:39 -0600

    homework

---
+++ dungeonController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dungeonController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        room r = new room
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- room.cs
+++ room.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class room
+public class Player
 {
     private string name;
-    private int exitNum;
-    public room(string name)
-    {
-        this.exitNum = (int)Random.Range(1,4);
-        this.name = name;
+    private int hp;
 
+    public Player(string name)
+    {
+        this.hp = (int)Random.Range(10.0f, 20.0f);
+        this.name = name;
     }
     public string display()
     {
